1c2113c01ea9478cf370cb44eaf255aec3e7309e
znb <znb20010119@163.com>
2022-07-04 07:56:18 +0800

    页面调试

--- pages/index.vue
+++ pages/index.vue
@@ -38,7 +38,7 @@
                         :alt="course.title"
                       >
                       <div class="cc-mask">
-                        <a href="#" title="开始学习" class="comm-btn c-btn-1">开始学习</a>
+                        <a :href="'/course/'+course.id" title="开始学习" class="comm-btn c-btn-1">开始学习</a>
                       </div>
                     </section>
                     <h3 class="hLh30 txtOf mt10">
